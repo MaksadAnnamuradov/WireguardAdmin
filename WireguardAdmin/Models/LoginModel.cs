dd7aba912c39f132b2c9a513b868951ff6fb3f2c
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-01-21 19:54:24 -0700

    first commit

---
+++ WireguardAdmin/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WireguardAdmin.Models
+{
+    public class LoginModel {
+        public string Name { get; set; }
+        public string IPAddress { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string AllowedIPRange { get; set; }
+        public string ClientPublicKey { get; set; }
+        public string ClientPrivateKey { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
---
+++ WireguardAdmin/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace WireguardAdmin.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string IPAddress { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string AllowedIPRange { get; set; }
+        public string ClientPublicKey { get; set; }
+        public string ClientPrivateKey { get; set; }
+    }
+}
+
+/*Client Name
+IP address
+Date Added
+Allowed IP Range
+Client public key
+Client private key*/
