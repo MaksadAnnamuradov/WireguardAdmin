db093fa951913e92ca28ea13ce23e9265c9f9e83
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-02-19 22:19:20 -0700

    added srcipt

---
+++ WireguardAdmin/Update.sh
@@ -0,0 +1,2 @@
+﻿#!/bin/bash
+git pull && systemctl stop WireguardAdmin.service &&  dotnet publish -c Release -o /var/WireguardAdmin && systemctl start WireguardAdmin.service && systemctl status WireguardAdmin.service
